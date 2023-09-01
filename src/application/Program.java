1d0bb29cb27a4d6d134fb88f77d2be6b516b9214
Reinaldo Almeida de Jesus <reinaldojesus238@gmail.com>
2023-09-01 16:20:08 -0300

    Little improvement in board printing

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,7 +4,7 @@ import boardgame.Board;
 import boardgame.Position;
 import chess.ChessMatch;
 
-public class MainProgram {
+public class Program {
     public static void main(String[] args) {
         ChessMatch chessMatch = new ChessMatch();
 
